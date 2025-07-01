b80771a23bfe40dab0ac5b19d810e383865c7f28
Soto2305 <medeleon@cooperativasalcaja.com>
2025-07-01 11:50:23 -0600

    chore(CrearCarta): Se quitaron los comentarios

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -7,7 +7,6 @@ export const crearCartaHTML = (carta) => {
   if (!carta) {
     throw new Error("La carta es siempre necesaria");
   }
-  // <img class="carta" src="assets/cartas/2C.png">
   const imgCarta = document.createElement("img");
   imgCarta.src = `assets/cartas/${carta}.png`; //3H, JD
   imgCarta.classList.add("carta");
